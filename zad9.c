a0b48a64acd37d567ece356e00e1679134658ff1
KortylaAdrianAGH <Akortyla@student.agh.edu.pl>
2019-11-13 13:12:27 +0100

    hey lol lol2

--- zad9.c
+++ zad9.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int main() {
-    int k,n,i,j,space; 
+    int n,i,j,space; 
     printf("wprowadz wysokosc piramidy: ");
     scanf("%i", &n);
     for (i=0;i<=n;i++){
@@ -8,11 +8,8 @@ int main() {
         for(space=0;space<n-i;space++){
             printf(" ");
         }   
-        for(j=0;j<i;j++){
+        for (j=0;j<2*i-1;j++){
             printf("%%");
-        }
-        for (k=0;k<i-1;k++){
-            printf("%%",k,i);
         } 
     printf("\n");
     }
